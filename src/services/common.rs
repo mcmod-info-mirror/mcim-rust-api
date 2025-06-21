b5086a1264d6f8b86922344fd816ad298e62084d
z0z0r4 <z0z0r4@outlook.com>
2025-06-21 16:58:37 +0800

    chore: 统计信息与 mcim-api 保持一致格式

--- src/services/common.rs
+++ src/services/common.rs
@@ -1,50 +1,75 @@
+use std::collections::HashMap;
+
 use crate::config::database::get_database_name;
 
 use crate::errors::ServiceError;
 
-async fn get_collection_count(db: &mongodb::Client, collection_name: &str) -> Result<u64, ServiceError> {
-    let collection = db.database(get_database_name().as_str()).collection::<mongodb::bson::Document>(collection_name);
+async fn get_collection_count(
+    db: &mongodb::Client,
+    collection_name: &str,
+) -> Result<u64, ServiceError> {
+    let collection = db
+        .database(get_database_name().as_str())
+        .collection::<mongodb::bson::Document>(collection_name);
 
     // 如果 $collStats 失败，回退到 estimated_document_count
-    let count = collection.estimated_document_count(None).await.map_err(|e| {
-            ServiceError::DatabaseError {
-                message: format!("Failed to get collection count for {}: {}", collection_name, e), source: Some(e.into()),
-            }
-    })?;
+    let count = collection
+        .estimated_document_count(None)
+        .await
+        .map_err(|e| ServiceError::DatabaseError {
+            message: format!(
+                "Failed to get collection count for {}: {}",
+                collection_name, e
+            ),
+            source: Some(e.into()),
+        })?;
     Ok(count)
 }
 
-pub async fn get_statistics_info(modrinth: bool, curseforge: bool, translate: bool, db: &mongodb::Client) -> Result<serde_json::Value, ServiceError> {
-    let mut statistics = serde_json::Map::new();
+pub async fn get_statistics_info(
+    modrinth: bool,
+    curseforge: bool,
+    translate: bool,
+    db: &mongodb::Client,
+) -> Result<serde_json::Value, ServiceError> {
+    let mut statistics = HashMap::new();
     if modrinth {
+        let mut modrinth_statistics = HashMap::new();
         let modrinth_projects_count = get_collection_count(db, "modrinth_projects").await?;
-        statistics.insert("modrinth_projects".to_string(), serde_json::Value::from(modrinth_projects_count));
+        modrinth_statistics.insert("project".to_string(), modrinth_projects_count);
 
         let modrinth_versions_count = get_collection_count(db, "modrinth_versions").await?;
-        statistics.insert("modrinth_versions".to_string(), serde_json::Value::from(modrinth_versions_count));
+        modrinth_statistics.insert("version".to_string(), modrinth_versions_count);
 
         let modrinth_files_count = get_collection_count(db, "modrinth_files").await?;
-        statistics.insert("modrinth_files".to_string(), serde_json::Value::from(modrinth_files_count));
+        modrinth_statistics.insert("file".to_string(), modrinth_files_count);
+
+        statistics.insert("modrinth".to_string(), modrinth_statistics);
     }
 
     if curseforge {
+        let mut curseforge_statistics = HashMap::new();
         let curseforge_mods_count = get_collection_count(db, "curseforge_mods").await?;
-        statistics.insert("curseforge_mods".to_string(), serde_json::Value::from(curseforge_mods_count));
+        curseforge_statistics.insert("mod".to_string(), curseforge_mods_count);
 
         let curseforge_files_count = get_collection_count(db, "curseforge_files").await?;
-        statistics.insert("curseforge_files".to_string(), serde_json::Value::from(curseforge_files_count));
+        curseforge_statistics.insert("file".to_string(), curseforge_files_count);
 
-        let curseforge_fingerprints_count = get_collection_count(db, "curseforge_fingerprints").await?;
-        statistics.insert("curseforge_fingerprints".to_string(), serde_json::Value::from(curseforge_fingerprints_count));
+        let curseforge_fingerprints_count =
+            get_collection_count(db, "curseforge_fingerprints").await?;
+        curseforge_statistics.insert("fingerprint".to_string(), curseforge_fingerprints_count);
+        statistics.insert("curseforge".to_string(), curseforge_statistics);
     }
 
     if translate {
+        let mut translate_statistics = HashMap::new();
         let curseforge_translate_count = get_collection_count(db, "curseforge_translated").await?;
-        statistics.insert("curseforge_translated".to_string(), serde_json::Value::from(curseforge_translate_count));
+        translate_statistics.insert("curseforge".to_string(), curseforge_translate_count);
 
         let modrinth_translate_count = get_collection_count(db, "modrinth_translated").await?;
-        statistics.insert("modrinth_translated".to_string(), serde_json::Value::from(modrinth_translate_count));
+        translate_statistics.insert("modrinth".to_string(), modrinth_translate_count);
+        statistics.insert("translate".to_string(), translate_statistics);
     }
 
-    Ok(serde_json::Value::Object(statistics))
+    Ok(serde_json::json!(statistics))
 }
